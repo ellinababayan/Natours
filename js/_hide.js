d95e6822eb3b53306dc733b49f03755f39f9a9a4
Ellina Babaian <128013006+ellinababayan@users.noreply.github.com>
2023-05-30 16:29:25 -0700

    Add files via upload

---
+++ js/_hide.js
@@ -0,0 +1,13 @@
+// document.getElementById("navigation__link").onclick = function Hide() {
+//     style.display = "none"
+// }
+// function Hide() 
+
+function hide() {
+    console.log('here');
+    document.getElementsByClassName('navigation__checkbox')[0].checked = false;
+    // hide the lorem ipsum text
+    // document.getElementById(id).style.display = 'none';
+    // hide the link
+    // btn.style.display = 'none';
+}
